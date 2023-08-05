1342a407d145f2be80fa17fc8cf46beefd75d644
Chia1104 <yuyuchia7423@gmail.com>
2023-08-06 05:25:06 +0800

    Revert "test"
    This reverts commit 95154932394e6d7e54983365d7c392a2cece9da5.

--- src/app/api/logto/sign-in-callback/route.ts
+++ src/app/api/logto/sign-in-callback/route.ts
@@ -1,9 +1,18 @@
 import { type NextRequest } from "next/server";
 
-import { logtoClientOverride } from "@/server/logto/logto-client";
+import { logtoClientEdge } from "@/server/logto/logto-client";
 
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  return await logtoClientOverride.handleSignInCallback()(request);
+  console.log(
+    "sign-in-callback/route.ts: GET - ",
+    "url: ",
+    request.url.toString(),
+    "nextUrl: ",
+    request.nextUrl.toString(),
+    "headers['host']: ",
+    request.headers.get("host")
+  );
+  return await logtoClientEdge.handleSignInCallback()(request);
 }
