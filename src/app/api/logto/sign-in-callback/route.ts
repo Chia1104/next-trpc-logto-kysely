b0d1750095834649f20e3a8179b9e3da1cbf6abc
Chia1104 <yuyuchia7423@gmail.com>
2023-08-06 02:37:37 +0800

    fix: why 'request.url' is correct in vercel?

--- src/app/api/logto/sign-in-callback/route.ts
+++ src/app/api/logto/sign-in-callback/route.ts
@@ -1,18 +1,24 @@
 import { type NextRequest } from "next/server";
-
+import { env } from "@/env.mjs";
+import { getBaseUrl } from "@/utils/get-base-url";
+import { redirect } from "next/navigation";
 import { logtoClientEdge } from "@/server/logto/logto-client";
 
 export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
-  console.log(
-    "sign-in-callback/route.ts: GET - ",
-    "url: ",
-    request.url.toString(),
-    "nextUrl: ",
-    request.nextUrl.toString(),
-    "headers['host']: ",
-    request.headers.get("host")
-  );
+  // rewrite the url to replace localhost:3000 with foo.zeabur.app
+  // this is needed because the callback url is set to localhost:3000 in docker
+  // and we need to redirect to foo.zeabur.app
+  if (
+    env.NODE_ENV === "production" &&
+    request.url.toString().includes(`http://localhost:${env.PORT}`)
+  ) {
+    redirect(
+      request.url
+        .toString()
+        .replace(`http://localhost:${env.PORT}`, getBaseUrl({ isServer: true }))
+    );
+  }
   return await logtoClientEdge.handleSignInCallback()(request);
 }
