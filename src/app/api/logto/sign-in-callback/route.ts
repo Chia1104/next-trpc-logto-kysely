dca9e1768ab842bcbcaa933d60450fb99f4d0ab3
Chia1104 <yuyuchia7423@gmail.com>
2023-10-31 17:18:15 +0800

    fix: disable edge function

--- src/app/api/logto/sign-in-callback/route.ts
+++ src/app/api/logto/sign-in-callback/route.ts
@@ -2,7 +2,7 @@ import { type NextRequest } from "next/server";
 
 import { logtoClientEdge } from "@/server/logto/logto-client";
 
-export const runtime = "edge";
+// export const runtime = "edge";
 
 export async function GET(request: NextRequest) {
   return await logtoClientEdge.handleSignInCallback()(request);
